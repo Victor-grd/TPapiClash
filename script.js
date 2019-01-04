53f2418279f053016b9c88fe68d629c31388e493
admin <admin@iMac3.local>
2019-01-04 16:29:51 +0100

    Bonne année

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var xhttp = new XMLHttpRequest();
  xhttp.onreadystatechange = function() {
    if (this.readyState == 4 && this.status == 200) {
      let cards = JSON.parse(this.response);
+
      for (let i = 0;i < cards.length; i++){
         let div = document.createElement('DIV');
         let img = document.createElement('IMG');
@@ -12,21 +13,29 @@ var xhttp = new XMLHttpRequest();
         img.style.width = "auto";
         img.src = "http://www.clashapi.xyz/images/cards/"+idName+".png";
 
+        if (cards[i].rarity == "Common") {
+            img.classList.add('common')
+        }else if (cards[i].rarity == "Rare") {
+            img.classList.add('rare')
+        }else if(cards[i].rarity == "Epic"){
+            img.classList.add('epic')
+        }else if (cards[i].rarity == "Legendary") {
+            img.classList.add('legendary')
+        }
+
         img.classList.add('cardsStyle')
         div.appendChild(img);
         div.id = cards[i].idName;
         document.querySelector('.cards').appendChild(div);
      }
-    //  document.querySelector('.deck').addEventListener('click', (el)=>{
-    //      debugger;
-    //     // deck.splice(, 1);
-    //  })
+
      document.querySelector('.cards').addEventListener('click', (el)=>{
         if(el.target.classList.contains('cardsStyle') && deck.length < 8){
             deck.push(el.target);
             let div = document.createElement('DIV');
             let img = document.createElement('IMG');
             img.src = el.target.src
+
             div.appendChild(img);
             img.classList.add('cardsStyle')
             img.classList.add('cardDeck')
@@ -50,12 +59,22 @@ document.querySelector('#buttonRandomDeck').addEventListener('click', ()=>{
             deck.push(randomDeck[i])
             let div = document.createElement('DIV');
             let img = document.createElement('IMG');
+            if (randomDeck[i].rarity == "Common") {
+                img.classList.add('common')
+            }else if (randomDeck[i].rarity == "Rare") {
+                img.classList.add('rare')
+            }else if(randomDeck[i].rarity == "Epic"){
+                img.classList.add('epic')
+            }else if (randomDeck[i].rarity == "Legendary") {
+                img.classList.add('legendary')
+            }
             img.src = "http://www.clashapi.xyz/images/cards/"+randomDeck[i].idName+".png";
             div.appendChild(img);
             img.classList.add('cardsStyle')
             img.classList.add('cardDeck')
-            document.querySelector('.deck').appendChild(div);
+            document.querySelector('.cardsDeck').appendChild(div);
         }
+        getElixirAverage()
       }
     };
     xhttp2.open("GET", "http://www.clashapi.xyz/api/random-deck", true);
@@ -69,11 +88,10 @@ function deleteCard(){
     }
 }
 
-
-
-
-
-
+function getElixirAverage(){
+    var result = ((deck[0].elixirCost+deck[1].elixirCost+deck[2].elixirCost+deck[3].elixirCost+deck[4].elixirCost+deck[5].elixirCost+deck[6].elixirCost+deck[7].elixirCost)/8)
+    document.querySelector('.AverageElixir').innerHTML = "Avergare elexir =" + result.toFixed(1);
+}
 
 
 
